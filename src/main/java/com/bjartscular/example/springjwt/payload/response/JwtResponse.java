4e76d344a8a3e041ee8a4e4836ae3d024abe2af4
bjartscular <iyakomissarenko@dev.vtb>
2021-11-13 20:41:37 +0300

    added TestController

--- src/main/java/com/bjartscular/example/springjwt/payload/response/JwtResponse.java
+++ src/main/java/com/bjartscular/example/springjwt/payload/response/JwtResponse.java
@@ -1,7 +1,14 @@
 package com.bjartscular.example.springjwt.payload.response;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
@@ -20,47 +27,4 @@ public class JwtResponse {
         this.roles = roles;
     }
 
-    public String getAccessToken() {
-        return token;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.token = accessToken;
-    }
-
-    public String getTokenType() {
-        return type;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.type = tokenType;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
 }
